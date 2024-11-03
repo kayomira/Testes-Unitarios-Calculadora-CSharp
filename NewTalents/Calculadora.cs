5f6e665c04815ca9ae27fd3fbf588f22fea790c4
kayomira <165169818+kayomira@users.noreply.github.com>
2024-11-03 17:15:37 -0300

    Projeto Finalizado

---
+++ NewTalents/Calculadora.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewTalents
+{
+    public class Calculadora{
+
+        private List<string> listahistorico;
+
+        public Calculadora() { 
+            listahistorico = new List<string>();
+        }
+
+        public int Somar(int val1, int val2) {
+            int res = val1 + val2;
+            listahistorico.Insert(0, "Res: " + res);
+            return res;
+        }
+        public int Subtrair(int val1, int val2)
+        {
+            int res = val1 - val2;
+            listahistorico.Insert(0, "Res: " + res);
+            return res;
+        }
+        public int Multiplicar(int val1, int val2)
+        {
+            int res = val1 * val2;
+            listahistorico.Insert(0, "Res: " + res);
+            return res;
+        }
+        public int Dividir(int val1, int val2)
+        {
+            int res = val1 / val2;
+            listahistorico.Insert(0, "Res: " + res);
+            return res;
+        }
+        public List<string> historico()
+        {
+            
+            listahistorico.RemoveRange(3,listahistorico.Count - 3);
+            return listahistorico;
+        }
+    }
+}
---
+++ TestNewTalents/UnitTest1.cs
@@ -0,0 +1,76 @@
+using NewTalents;
+namespace TestNewTalents
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(4, 5, 9)]
+        public void TestSomar(int val1, int val2, int resultado)
+        {
+            Calculadora calc = new Calculadora();
+
+            int resultadoCalculadora = calc.Somar(val1, val2);
+
+            Assert.Equal(resultado, resultadoCalculadora);
+        }
+
+        [Theory]
+        [InlineData(1, 2, 2)]
+        [InlineData(4, 5, 20)]
+        public void TestMultiplicar(int val1, int val2, int resultado)
+        {
+            Calculadora calc = new Calculadora();
+
+            int resultadoCalculadora = calc.Multiplicar(val1, val2);
+
+            Assert.Equal(resultado, resultadoCalculadora);
+        }
+
+        [Theory]
+        [InlineData(6, 2, 3)]
+        [InlineData(10, 5, 2)]
+        public void TestDividir(int val1, int val2, int resultado)
+        {
+            Calculadora calc = new Calculadora();
+
+            int resultadoCalculadora = calc.Dividir(val1, val2);
+
+            Assert.Equal(resultado, resultadoCalculadora);
+        }
+
+        [Theory]
+        [InlineData(6, 2, 4)]
+        [InlineData(10, 5, 5)]
+        public void TestSubtrair(int val1, int val2, int resultado)
+        {
+            Calculadora calc = new Calculadora();
+
+            int resultadoCalculadora = calc.Subtrair(val1, val2);
+
+            Assert.Equal(resultado, resultadoCalculadora);
+        }
+        [Fact]
+        public void TestarDivisaoPorZero()
+        {
+            Calculadora calc = new Calculadora();
+
+            Assert.Throws<DivideByZeroException>(() => calc.Dividir(3,0));
+        }
+
+        public void TestarHistorico()
+        {
+            Calculadora calc = new Calculadora();
+
+            calc.Somar(1, 2);
+            calc.Somar(2, 8);
+            calc.Somar(3, 7);
+            calc.Somar(4, 1);
+
+            var lista = calc.historico();
+
+            Assert.NotEmpty(calc.historico());
+            Assert.Equal(3, lista.Count);
+        }
+    }
+}
